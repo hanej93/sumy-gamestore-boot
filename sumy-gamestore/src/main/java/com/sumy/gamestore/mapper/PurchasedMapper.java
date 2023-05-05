1efbec10eebf801e54aacd455ecfaba3ff910f09
EJ <hanej93@naver.com>
2023-05-05 19:42:36 +0900

    PurchasedMapper xml로 이관

--- sumy-gamestore/src/main/java/com/sumy/gamestore/mapper/PurchasedMapper.java
+++ sumy-gamestore/src/main/java/com/sumy/gamestore/mapper/PurchasedMapper.java
@@ -15,18 +15,12 @@ import com.sumy.gamestore.model.PurchasedGameList;
 @Mapper
 public interface PurchasedMapper {
 
-	@Select("select * from purchased_game_list as p "
-			+ "join game_info as g "
-			+ "on p.game_id = g.game_id where user_id = #{userId} order by purchased_game_id desc ")
 	public List<PurchasedGameInfoDto> selectPurchasedGameListByUserId(int userId);
 
-	@Insert("insert into purchased_game_list values(#{purchasedGameId}, #{gameId}, #{userId}, #{purchasedWriteDate})")
 	public int insertPurchasedGameList(PurchasedGameList purchasedGameList);
 	
-	@Select("select count(*) from purchased_game_list where user_id=#{userId} and game_id=#{gameId}")
 	public int countPurchasedGameListByIds(@Param(value = "userId") int userId,@Param(value = "gameId") int gameId);
 	
-	@Delete("delete from purchased_game_list where user_id=#{userId} and game_id=#{gameId}")
 	public int deletePurchasedGameListByIds(@Param(value = "userId") int userId,@Param(value = "gameId") int gameId);
 
 	// ================================================
