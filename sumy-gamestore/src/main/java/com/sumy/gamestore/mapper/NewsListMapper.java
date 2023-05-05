86ec72aa3bf856f74ef4f70b8951d1d324178896
EJ <hanej93@naver.com>
2023-05-05 18:59:22 +0900

    NewsListMapper xml로 이관

--- sumy-gamestore/src/main/java/com/sumy/gamestore/mapper/NewsListMapper.java
+++ sumy-gamestore/src/main/java/com/sumy/gamestore/mapper/NewsListMapper.java
@@ -13,27 +13,13 @@ import com.sumy.gamestore.model.NewsList;
 
 @Mapper
 public interface NewsListMapper {
-	@Insert("insert into news_list values(#{newsId}, #{newsTitle}, #{newsSubTitle}, #{newsThumbImage},"
-			+ "#{newsText}, #{newsWriteDate}, #{newsUpdateDate})")
-	public int insertNews(NewsList News);
 
-	@Select("select * from news_list where news_id = #{newsId}")
-	public NewsList selectOneByNewsId(int newsId);
-	
-	@Update("update news_list set news_title = #{newsTitle}, news_sub_title = #{newsSubTitle}, news_thumb_image = #{newsThumbImage}, "
-			+ "news_text = #{newsText}, news_write_date = #{newsWriteDate}, news_update_date = #{newsUpdateDate} "
-			+ "where news_id = #{newsId}")
-	public int updateNews(NewsList News);
-	
-	@Delete("delete from news_list where news_id = #{newsId}")
-	public int deleteNews(int newsId);
-	
-	// ================================================
-	
-//	@Select("select count(*) from news_list")
 	public int countNewsList(PagingVO vo);
-
-//	@Select("select * from news_list order by news_id desc limit #{start}, #{cntPage}")
 	public List<NewsList> selectNewsList(PagingVO vo);
-	
+
+	public int insertNews(NewsList News);
+	public NewsList selectOneByNewsId(int newsId);
+	public int updateNews(NewsList News);
+	public int deleteNews(int newsId);
+
 }
