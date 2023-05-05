1ad4bf5da3ad2cfdaae37a0f9b12aa2645dbf88a
EJ <hanej93@naver.com>
2023-05-05 20:04:39 +0900

    ReportListMapper xml 로 이관

--- sumy-gamestore/src/main/java/com/sumy/gamestore/mapper/ReportListMapper.java
+++ sumy-gamestore/src/main/java/com/sumy/gamestore/mapper/ReportListMapper.java
@@ -14,23 +14,11 @@ import com.sumy.gamestore.model.ReportList;
 
 @Mapper
 public interface ReportListMapper {
-	
-	@Select("select * from report_list where report_id = #{reportId}")
+
 	public ReportList selectOneByReportId(int reportId);
-	
-	@Insert("insert into report_list values(#{reportId}, #{reportFromUserId}, #{reviewId},"
-			+ " #{reportText}, #{reportWriteDate}, #{reportReadYn})")
 	public int insertReport(ReportList reportList);
-	
-	@Update("update report_list set report_from_user_id=#{reportFromUserId}, review_id = #{reviewId},"
-			+ " report_text = #{reportText}, report_write_date = #{reportWriteDate},"
-			+ " report_read_yn = #{reportReadYn} where report_id = #{reportId}")
 	public int updateReport(ReportList reportList);
-	
-	@Delete("delete from report_list where report_id = #{reportId}")
 	public int deleteReport(int reportId);
-	
-	@Select("select count(*) from report_list where review_id = #{reviewId}")
 	public int countReportByReviewId(int reviewId);
 	
 	// ================================================
